8edae5dab2268c56e7d2d47f9a60165937627c4d
Luis Montenegro <lem28@njit.edu>
2018-02-08 17:03:20 -0500

    Commit #12: cleaned up sql files

--- sql/files_table.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists files;
-create table files
-(
-	file_id int(128) primary key auto_increment,
-	file_name text,
-	file_url text,
-    upload_date datetime,
-    expiration_date datetime
-);
