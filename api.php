dcc689891d6c4e39f1653e10748de30b7bd07383
Luis Montenegro <lem28@njit.edu>
2018-03-05 01:59:32 -0500

    Commit #44: I love it when shit works. Please - PLEASE - don't fucking break this or I'll break you.

--- api.php
+++ api.php
@@ -1,5 +1,6 @@
 <?php
   $q = $_REQUEST["q"];
+  $outputFile = 'output.json';
   $name = str_replace(" ", '%20', $q);
   $baseURL = "https://www.giantbomb.com/api/search/?api_key=";
   $apiKey = "1eb68d69b5b8c3a4d37f93116ba4968ccf789a33";
@@ -7,8 +8,21 @@
   $fileFormat = "&format=json";
   $resourceType = "&resources=game";
   $fullURL = $baseURL.$apiKey.$fileFormat.$query.'"'.$name.'"'.$resourceType."";
-  $options  = array('http' => array('user_agent' => 'custom user agent string'));
-  $context  = stream_context_create($options);
-  $response = file_get_contents($fullURL, false, $context);
-  echo $response;
+  $options = array('http' => array('user_agent' => 'custom user agent string'));
+  $context = stream_context_create($options);
+  $apiResult = file_get_contents($fullURL, false, $context);
+  $json = json_decode($apiResult);
+  echo "<a href=\"".$json->results[0]->site_detail_url."\" target=\"_blank\"><img src=\"".$json->results[0]->image->screen_url."\"></a>";
+  echo "<br>";
+  echo "Name: ".$json->results[0]->name;
+  echo "<br>";
+  if ($json->results[0]->expected_release_year == null)
+  {
+  	echo "Original Release Date: ".$json->results[0]->original_release_date;
+  } else
+  {
+  	echo "Expected Release Date: ".$json->results[0]->expected_release_month." - ".$json->results[0]->expected_release_day." - ".$json->results[0]->expected_release_year;
+	}
+  echo "<br>";
+  echo $json->results[0]->deck;
 ?>
--- search.php
+++ search.php
@@ -10,45 +10,46 @@
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 	<link href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
 	<link href="css/style.css" rel="stylesheet">
-  <script src="js/index.js"></script>
-  <?php include 'css/css.html';?>
+	<script src="js/index.js"></script>
+	<?php include 'css/css.html';?>
 </head>
 <body>
 	<div class="form">
-		<h1>Search PC games</h1>
-		<h2><?php echo $first_name.' '.$last_name;?></h2>
-		<h2><?=$email?></h2>
-		<div class="wrapper">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<header id="header">
-							<div class="slider">
-								<div class="carousel slide" data-ride="carousel" id="carousel-example-generic"></div>
-								<div class="collapse navbar-collapse" id="mainNav">
-                </div>
-							</div>
-							<script>
-			           function showHint(str) {
-		                 var xhr = new XMLHttpRequest();
-		                 xhr.onreadystatechange = function()
-                     {
-		                     if (this.readyState == 4 && this.status == 200)
-                         {
-		                         document.getElementById("result").innerHTML = this.responseText;
-		                     }
-		                 };
-		                 xhr.open("GET", "api.php?q=" + str, true);
-                     xhr.send();
-			           }
-							</script>
-							<div>
-                <p>Start typing a name in the input field below:</p>
-                <input onkeyup="showHint(this.value)" type="text">
-                <div id="result"></div>
-							</div>
-						</header>
-            <a href="profile.php"><button class="button button-block" name="HOME">HOME</button></a>
+	<h1>Search PC games</h1>
+	<h2><?php echo $first_name.' '.$last_name;?></h2>
+	<h2><?=$email?></h2>
+	<div class="wrapper">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<header id="header">
+						<div class="slider">
+							<div class="carousel slide" data-ride="carousel" id="carousel-example-generic"></div>
+							<div class="collapse navbar-collapse" id="mainNav"></div>
+						</div>
+				<script>
+					var apiResult;	
+					function showHint(str) {
+					var xhr = new XMLHttpRequest();
+					xhr.onreadystatechange = function()
+						{
+							if (this.readyState == 4 && this.status == 200)
+							{
+								apiResult = this.responseText;
+								document.getElementById("result").innerHTML = apiResult;
+							}
+						};
+						xhr.open("GET", "api.php?q=" + str, true);
+						xhr.send();
+					}
+				</script>
+				<div>
+					<p>Start typing a name in the input field below:</p>
+					<input onkeyup="showHint(this.value)" type="text">
+					<div id="result"></div>
+				</div>
+				</header>
+				<a href="profile.php"><button class="button button-block" name="HOME">HOME</button></a>
 					</div>
 				</div>
 			</div>
